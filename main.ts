f63ade312d45f29fa7616e21019986eab2f2fe2c
PatcharaP <itt.patchara@gmail.com>
2025-11-28 13:12:06 +0700

    feat:all core func for hardcode board

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ type Tile = "*" | "░" | "O" | "^";
 type Board = Tile[][];
 
 const prompt = promptSync({ sigint: true });
-
+const vaildInput = ["w", "a", "s", "d"];
 // Board tiles
 const PLAYER: Tile = "*";
 const EMPTY: Tile = "░";
@@ -14,24 +14,95 @@ const HAT: Tile = "^";
 
 // Hardcoded board
 let board: Board = [
-	[PLAYER, EMPTY, HOLE],
-	[EMPTY, HOLE, EMPTY],
-	[EMPTY, HAT, EMPTY],
+  [PLAYER, EMPTY, HOLE],
+  [EMPTY, HOLE, EMPTY],
+  [EMPTY, HAT, EMPTY],
 ];
 
-//  Game state 
+//  Game state
 let playerRow: number = 0;
 let playerCol: number = 0;
 let playing: boolean = true;
 
+let isFalloff: boolean = false;
+let isHole: boolean = false;
+let isHat: boolean = false;
+
 // Print board
 function printBoard(board: Board): void {
-	console.clear();
-	console.log(board);
+  console.clear();
+  board.forEach((row) => {
+    console.log(row.join(""));
+  });
 }
 
-// Game play loop
-printBoard(board);
-const input:string = prompt("Which way? (w/a/s/d): ");
-console.log(input);
+function getInput(): string {
+  while (true) {
+    const input: string = prompt("Which way? (w/a/s/d): ").toLowerCase();
+    if (vaildInput.includes(input)) return input;
+    console.log(`invalid movement`);
+  }
+}
+function moveUp(): void {
+  playerRow--;
+  handleTile();
+}
+function moveDown(): void {
+  playerRow++;
+  handleTile();
+}
+function moveLeft(): void {
+  playerCol--;
+  handleTile();
+}
+function moveRight(): void {
+  playerCol++;
+  handleTile();
+}
+function movePlayer(input: string): void {
+  switch (input) {
+    case "w":
+      if (playerRow > 0) {
+        moveUp();
+      } else isFalloff = true;
+      break;
+    case "s":
+      if (playerRow < board.length - 1) {
+        moveDown();
+      } else isFalloff = true;
+      break;
+    case "a":
+      if (playerCol > 0) {
+        moveLeft();
+      } else isFalloff = true;
+      break;
+    case "d":
+      if (playerCol < board[0].length - 1) {
+        moveRight();
+      } else isFalloff = true;
+      break;
+  }
+}
 
+function handleTile(): void {
+  if (board[playerRow][playerCol] === HAT) isHat = true;
+  if (board[playerRow][playerCol] === HOLE) isHole = true;
+  board[playerRow][playerCol] = PLAYER;
+}
+
+function ruleCheck() : void {
+  if (isFalloff || isHat || isHole) playing = false;
+}
+
+function gameEnd(): void {
+  if (isHole) console.log(`Game Over! : You fall off the hole!`);
+  if (isFalloff) console.log(`Game Over! : You Out of bound!`);
+  if (isHat) console.log(`You found Hat, You Win!`);
+}
+// Game play loop
+do {
+  printBoard(board);
+  movePlayer(getInput());
+  ruleCheck();
+} while (playing);
+gameEnd();
